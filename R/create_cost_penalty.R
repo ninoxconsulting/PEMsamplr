1665ce0b4691bb17ddafba950c009933494d56cb
Genevieve Perkins <39040813+gcperk@users.noreply.github.com>
2024-12-06 14:19:02 -0800

    Update R/create_cost_penalty.R
    Co-authored-by: Andy Teucher <andy.teucher@gmail.com>

--- R/create_cost_penalty.R
+++ R/create_cost_penalty.R
@@ -63,8 +63,8 @@ create_cost_penalty <- function(vec_dir = fs::path(PEMprepr::read_fid()$dir_1010
                                 out_dir = fs::path(PEMprepr::read_fid()$dir_201010_inputs$path_abs),
                                 write_output = TRUE) {
 
-  if (!inherits(vec_dir, c("character"))) {
-    cli::cli_abort("{.var vec_dir} must be a SpatRaster or a path to a file")
+if (!inherits(vec_dir, "character") || !fs::dir_exists(vec_dir)) {
+    cli::cli_abort("{.var vec_dir} must be a directory path")
   }
 
   if (inherits(dem, c("character"))) {
