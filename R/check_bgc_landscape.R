b31f9b7890807237514853136c87fe8a0baeb920
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-12-09 13:34:53 -0800

    stlyr

--- R/check_bgc_landscape.R
+++ R/check_bgc_landscape.R
@@ -4,8 +4,8 @@
 #'      scale. This output is derived from the create_bgc_template().
 #' @param binned_landscape A `SpatRast` with the landscape binned. This is the
 #'      output of the [create_binned_landscape()] function.
+#' @param plot a logical if the plot is to be returned. Default is true
 #' @return A dataframe with summary of landsclass class and BEC unit.
-#'        A plot is also returned.
 #' @export
 #'
 #' @examples
@@ -23,24 +23,18 @@ check_bgc_landscapes <- function(
     ),
     binned_landscape = fs::path(
       PEMprepr::read_fid()$dir_1020_covariates$path_rel,
-      "25m", "modules_landscape", "landscape_binned.tif"
-    )) {
+      "25m", "modules", "landscape_binned.tif"
+    ),
+    plot = TRUE) {
   # testing
   # bec <- fs::path(PEMprepr::read_fid()$dir_1020_covariates$path_rel, "25m", "bec.tif")
   # binned_landscape <- landscapes
   # binned_landscape = fs::path(PEMprepr::read_fid()$dir_1020_covariates$path_rel,"25m","modules_landscape")
 
-  if (inherits(bec, c("character"))) {
-    bec <- terra::rast(bec)
-  } else if (!inherits(bec, c("SpatRaster"))) {
-    cli::cli_abort("{.var bec} must be a SpatRaster or a path to a file")
-  }
 
-  if (inherits(binned_landscape, c("character"))) {
-    binned_landscape <- terra::rast(binned_landscape)
-  } else if (!inherits(binned_landscape, c("SpatRaster"))) {
-    cli::cli_abort("{.var binned_landscape} must be a SpatRaster or a path to a file")
-  }
+  bec <- PEMprepr:::read_spatrast_if_necessary(bec)
+
+  binned_landscape <- PEMprepr:::read_spatrast_if_necessary(binned_landscape)
 
   if (!isTRUE(terra::compareGeom(bec, binned_landscape))) {
     cli::cli_abort("{.var bec} must match spatial extent of landscapes raster stack")
@@ -51,10 +45,13 @@ check_bgc_landscapes <- function(
 
     routdf <- stats::na.omit(routdf)
 
-    ggplot2::ggplot(routdf, ggplot2::aes(routdf$landscape)) +
+  if(plot){
+    plot1 <- ggplot2::ggplot(routdf, ggplot2::aes(routdf$landscape)) +
       ggplot2::geom_histogram() +
       ggplot2::facet_wrap(~MAP_LABEL)
 
+    print(plot1)
+  }
     return(routdf)
   }
 }
