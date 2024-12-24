495ddd72593f7215c476b38a8b115048aed74c2c
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-12-24 10:51:17 -0800

    add convert lines to pt fn

---
+++ R/convert_lines_pts.R
@@ -0,0 +1,127 @@
+#' Convert Line segments or points to rasterized training points
+#'
+#' @param processed_lines A `sf` object of all attributed line segments or points
+#' @param trast A `SpatRast` or path to file with resolution matching the modelling resolution.
+#' @param neighbours A `logical` if adjacent neighbouring cells should be added. Default is FALSE.
+#' @param buffer A `numeric`to determine the extend in meters the line segments will be expanded to.
+#' Default is half of the raster template resolution 2.5m for a 5m resolution raster template
+#' @param write_output A `logical`if the sf object be written to disk?
+#'     If `TRUE` (default), will write to `out_dir` under the appropriate resolution subfolder.
+#' @param out_dir A character string of path which points to output location. A default
+#'    location and name are applied in line with standard workflow.
+#' @param out_name A character string of the output file name. Default is `allpoints.gpkg`
+#' @return sf object points based on raster reolution
+#' @export
+#' @examples
+#' \dontrun{
+#' clean_tracks <- convert_to_pts(
+#'   processed_transects = fs::path(PEMprepr::read_fid()$dir_20105020_clean_field_data$path_rel,
+#'   "proc_s1_transects5.gpkg"),
+#'   trast = fs::path(PEMprepr::read_fid()$dir_1020_covariates$path_rel, "5m", "template.tif"),
+#'   buffer = 2.5,
+#'   neighbours = FALSE,
+#'   write_output = TRUE,
+#'   out_dir = PEMprepr::read_fid()$dir_20105020_clean_field_data$path_rel,
+#'   out_name = "allpoints.gpkg")
+#' }
+convert_lines_pts <- function(processed_lines = fs::path(PEMprepr::read_fid()$dir_20105020_clean_field_data$path_rel, "proc_s1_transects5.gpkg"),
+                              trast,
+                              buffer = 2.5,
+                              neighbours = FALSE,
+                              write_output = TRUE,
+                              out_dir = PEMprepr::read_fid()$dir_20105020_clean_field_data$path_rel,
+                              out_name = "allpoints.gpkg") {
+
+  # check processed_transects is path or spatvect
+  processed_lines <- PEMprepr:::read_sf_if_necessary(processed_lines)
+
+  # check trast is path or spatrast
+  trast <- PEMprepr:::read_spatrast_if_necessary(trast)
+  tname <- names(trast)
+
+  # check if ID column exists
+  if ("ID" %in% colnames(processed_lines) == FALSE) {
+    processed_lines <- processed_lines |>
+      dplyr::mutate(ID = seq(1, length(processed_lines$order), 1))
+  }
+
+  processed_transects_id <- sf::st_drop_geometry(processed_lines)
+
+  geom_type <- as.character(unique(sf::st_geometry_type(processed_lines, by_geometry = TRUE)))
+
+  if (geom_type %in% c("LINESTRING")) {
+    # for lines
+    lBuff <- sf::st_buffer(processed_lines, dist = buffer, endCapStyle = "FLAT", joinStyle = "MITRE")
+    lBuff <- sf::st_cast(lBuff, "MULTIPOLYGON")
+  } else {
+    # for points
+    lBuff <- sf::st_buffer(processed_lines, dist = buffer)
+    lBuff <- sf::st_cast(lBuff, "MULTIPOLYGON")
+  }
+
+  # extract the XY values of raster cells where crossed lines
+  xys <- terra::extract(trast, terra::vect(lBuff), xy = TRUE)
+
+  raster_points_xy <- xys |>
+    sf::st_as_sf(coords = c("x", "y"), crs = 3005) |>
+    terra::merge(processed_transects_id) |>
+    dplyr::select(c(-"ID", -tname))
+
+  # add slice and tid (transect id)
+  allpts <- raster_points_xy |>
+    dplyr::mutate(tid = tolower(gsub("_[[:alpha:]].*", "", .data$transect_id))) |>
+    dplyr::mutate(slice = sub(".*(?=.$)", "", gsub("\\..*", "", .data$tid), perl = T))
+
+ # add neighbours if selected
+  if (neighbours) {
+    sf::st_geometry(allpts) <- "geom"
+    cli::cat_line()
+    cli::cli_alert_warning("generating neighbouring points")
+
+    dat_pts <- allpts
+
+    dat_pts$ptsID <- 1:nrow(dat_pts)
+    dat_atts <- data.table::as.data.table(sf::st_drop_geometry(dat_pts))
+    pts <- terra::vect(dat_pts)
+    cellNums <- terra::cells(trast, pts)
+    cell_lookup <- data.table::data.table(ID = pts$ptsID, cell = cellNums)
+
+    adjCells <- terra::adjacent(trast, cells = cellNums[, 2], directions = "queen", include = T)
+    adjCells <- data.table::as.data.table(adjCells)
+    data.table::setnames(adjCells, c("Orig", paste("Adj", 1:8, sep = "")))
+
+    adjCells[, ID := 1:nrow(adjCells)]
+    adjLong <- data.table::melt(adjCells, id.vars = "ID", value.name = "CellNum", variable.name = "Position")
+    data.table::setorder(adjLong, "ID", "Position")
+    terra::values(trast) <- 1:terra::ncell(trast)
+    cellnums <- 1:terra::ncell(trast)
+    trast[!cellnums %in% adjLong$CellNum] <- NA
+
+    pts <- terra::as.points(trast, values = T, na.rm = T)
+    pts2 <- sf::st_as_sf(pts)
+    pts2 <- data.table::as.data.table(pts2)
+    data.table::setnames(pts2, c("CellNum", "geometry"))
+    allPts <- terra::merge(pts2, adjLong, by = "CellNum", all = T)
+    # allPts[cell_lookup, ptID := i.ID, on = c(CellNum = "cell.cell")]
+    allPts <- terra::merge(allPts, dat_atts, by.x = "ID", by.y = "ptsID", all = T)
+    allPts <- terra::vect(sf::st_as_sf(allPts))
+    allPts <- sf::st_as_sf(allPts)
+    allpts <- dplyr::select(allPts, -"CellNum", -"ID")
+  }
+
+  if (write_output) {
+    out_loc <- fs::path(out_dir, out_name)
+
+    # if file exists
+    if (fs::file_exists(out_loc)) {
+      cli::cat_line()
+      cli::cli_alert_warning("file already exists at {.var {out_dir}}, this file will be overwriten")
+    }
+
+    sf::st_write(allpts, out_loc, driver = "GPKG", append = FALSE)
+    cli::cat_line()
+    cli::cli_alert_success("field data formatted and written to {.var {out_dir}}")
+  }
+
+  return(allpts)
+}
