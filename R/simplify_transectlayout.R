9fe7f01ee61ea55f8cc1345751e7f9a90ee596ca
Genevieve Perkins <39040813+gcperk@users.noreply.github.com>
2025-02-03 09:29:34 -0800

    Update R/simplify_transectlayout.R
    Co-authored-by: Andy Teucher <andy.teucher@gmail.com>

--- R/simplify_transectlayout.R
+++ R/simplify_transectlayout.R
@@ -5,7 +5,7 @@
 #'  multiple files so ensure any `.gpkg` files in this folder are part of the sample plan.
 #' @param out_dir A path to the location in which the simplified transect layout will be
 #' saved. Default location is based on standard workflow.
-#' @param write_output should the simplifeid transect layout sf object be written to disk?
+#' @param write_output should the simplified transect layout sf object be written to disk?
 #'     If `TRUE` (default), will write to `out_dir`. Default location is based
 #'     on standard workflow.
 #' @param overwrite a `logical` to determine if the output file be overwritten
